a54b32350815e89ce1cbc8e1de20a99282ff1d0d
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2020-12-14 21:25:22 +0530

    Added Implementation for external dependency

--- CreditCardApplications/CreditCardApplicationEvaluator.cs
+++ CreditCardApplications/CreditCardApplicationEvaluator.cs
@@ -5,12 +5,22 @@
         private const int AutoReferralMaxAge = 20;
         private const int HighIncomeThreshold = 100_000;
         private const int LowIncomeThreshold = 20_000;
-     
+        private IFrequentFlyerNumberValidator _validator;
+
+        public CreditCardApplicationEvaluator(IFrequentFlyerNumberValidator validator)
+        {
+            _validator = validator;
+        }
         public CreditCardApplicationDecision Evaluate(CreditCardApplication application)
         {
             if (application.GrossAnnualIncome >= HighIncomeThreshold)
             {
                 return CreditCardApplicationDecision.AutoAccepted;
+            }
+            var isValidFFNumber = _validator.IsValid(application.FrequentFlyerNumber);
+            if (!isValidFFNumber)
+            {
+                return CreditCardApplicationDecision.ReferredToHuman;
             }
 
             if (application.Age <= AutoReferralMaxAge)
