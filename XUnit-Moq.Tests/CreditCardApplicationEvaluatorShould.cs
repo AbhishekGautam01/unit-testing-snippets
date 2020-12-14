f897bca25bffdd19e54caa732c83c1e110b49d61
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2020-12-14 21:51:36 +0530

    COnfiguring MOCK

--- XUnit-Moq.Tests/CreditCardApplicationEvaluatorShould.cs
+++ XUnit-Moq.Tests/CreditCardApplicationEvaluatorShould.cs
@@ -1,4 +1,5 @@
 using CreditCardApplications;
+using Moq;
 using System;
 using Xunit;
 
@@ -9,7 +10,8 @@ namespace XUnit_Moq.Tests
         [Fact]
         public void AcceptHighIncomeApplication()
         {
-            var sut = new CreditCardApplicationEvaluator();
+            Mock<IFrequentFlyerNumberValidator> mockValidator = new Mock<IFrequentFlyerNumberValidator>();
+            var sut = new CreditCardApplicationEvaluator(mockValidator.Object);
             var application = new CreditCardApplication { GrossAnnualIncome = 100_100 };
             CreditCardApplicationDecision decision = sut.Evaluate(application);
             Assert.Equal(CreditCardApplicationDecision.AutoAccepted, decision);
@@ -18,10 +20,22 @@ namespace XUnit_Moq.Tests
         [Fact]
         public void ReferYoungApplications()
         {
-            var sut = new CreditCardApplicationEvaluator();
+            Mock<IFrequentFlyerNumberValidator> mockValidator = new Mock<IFrequentFlyerNumberValidator>();
+            var sut = new CreditCardApplicationEvaluator(mockValidator.Object);
             var application = new CreditCardApplication { Age = 19 };
             CreditCardApplicationDecision decision = sut.Evaluate(application);
             Assert.Equal(CreditCardApplicationDecision.ReferredToHuman, decision);
+        }
+
+        [Fact]
+        public void DeclineLowIncomeApplications()
+        {
+            Mock<IFrequentFlyerNumberValidator> mockValidator = new Mock<IFrequentFlyerNumberValidator>();
+            mockValidator.Setup(x => x.IsValid("x")).Returns(true);
+            var sut = new CreditCardApplicationEvaluator(mockValidator.Object);
+            var appliction = new CreditCardApplication { GrossAnnualIncome = 19_999, Age = 42, FrequentFlyerNumber = "x" };
+            CreditCardApplicationDecision decision = sut.Evaluate(appliction);
+            Assert.Equal(CreditCardApplicationDecision.AutoDeclined, decision);
         }
     }
 }
